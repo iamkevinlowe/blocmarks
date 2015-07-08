048a57044061d1474ab35c578fabbdc4d5bf42db
Kevin Lowe <iamkevinlowe@gmail.com>
2015-07-08 10:11:42 -0700

    Friendly Id Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  extend FriendlyId
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :topics
+
+  before_create :generate_slug
+
+  friendly_id :name, use: :slugged
+  validates_presence_of :name, :slug
+
+  private
+
+  def generate_slug
+    self.slug = name.parameterize
+  end
+
 end
