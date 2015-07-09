142ac2c0b22fccff4b0fb6b2901eef46cc38f93e
Kevin Lowe <iamkevinlowe@gmail.com>
2015-07-08 22:56:04 -0700

    User show page updated to show bookmarks they've created and bookmarks they've liked.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,15 @@ user = User.new(
   )
 user.skip_confirmation!
 user.save
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
 users = User.all
 
 # Create Topics
